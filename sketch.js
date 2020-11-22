ce01979f149f9c4fe5d28c1fd991271f8b3abc0f
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-11-22 17:45:09 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-
-var block1,block2,block3,block4,block5,block6,block7,block8,block9;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -21,31 +19,20 @@ function setup() {
 	ground = new Ground(400,600,150,10);
 	ground.shapeColor=color(182, 64, 65)
 
+	block = new Block(340,575,30,40)
+	block1 = new Block(370,575,30,40)
+	block2 = new Block(400,575,30,40)
+	block3 = new Block(430,575,30,40)
+	block4 = new Block(460,575,30,40)
+	block5 = new Block(370,535,30,40)
+	block6 = new Block(400,535,30,40)
+	block7 = new Block(430,535,30,40)
+	block8 = new Block(400,495,30,40)
 hexagon = createSprite(140,575,30,30)
 hexagon.shapeColor=color(250, 255, 0)
 
-block1 = createSprite(340,575,30,40)
-block1.shapeColor=color(255, 0, 0)
-block2 =  createSprite(370,575,30,40)
-block2.shapeColor=color(0, 0, 255)
-block3 =  createSprite(400,575,30,40)
-block3.shapeColor=color(255,0,0)
-block4 =  createSprite(430,575,30,40)
-block4.shapeColor=color(0,0,255)
-block5 =  createSprite(460,575,30,40)
-block5.shapeColor=color(255,0,0)
-
-block6 =  createSprite(370,535,30,40)
-block6.shapeColor=color(255,0,0)
-block7 =  createSprite(400,535,30,40)
-block7.shapeColor=color(0,0,255)
-block8 =  createSprite(430,535,30,40)
-block8.shapeColor=color(255,0,0)
-
-block9 =  createSprite(400,495,30,40)
-block9.shapeColor=color(255,0,0)
-
 	Engine.run(engine);
+
   
 }
 
@@ -54,6 +41,15 @@ function draw() {
   rectMode(CENTER);
   background(0);
   ground.display()
+  block.display()
+  block1.display()
+  block2.display()
+  block3.display()
+  block4.display()
+  block5.display()
+  block6.display()
+  block7.display()
+  block8.display()
   
   drawSprites();
  
